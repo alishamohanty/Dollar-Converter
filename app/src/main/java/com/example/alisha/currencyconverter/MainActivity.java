eb2a06f0f453fba4431cb5d048af631420f7143d
Alisha Mohanty <alishamohanty1416@gmail.com>
2018-08-25 14:47:24 +0530

    Fetched string is converted to Dollars

--- app/src/main/java/com/example/alisha/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/alisha/currencyconverter/MainActivity.java
@@ -4,11 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public void convert(View view){
+        EditText dollars = (EditText) findViewById(R.id.dollars);
+        String dollars_string = dollars.getText().toString(); //getting the string
+        Double dollars = Double.parseDouble(dollars_string); //converting to double
+        Log.i("amount", dollars);
+        Log.i("text","Convert clicked!!");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
